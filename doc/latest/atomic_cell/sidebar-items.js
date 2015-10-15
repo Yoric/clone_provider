ab36540fa4571d32e6b53d2abc1a495f6a2afd7f
David Rajchenbach-Teller <D.O.Teller@gmail.com>
2015-10-15 13:58:17 +0200

    Doc bump

---
+++ doc/latest/atomic_cell/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["AtomicCell","A cell holding values protected by an atomic lock."],["CleanGuard","A guard used to drop the value held by a `StaticCell` at a deterministic point in code."],["StaticCell","A cell holding values protected by an atomic lock."]]});
